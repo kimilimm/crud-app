175047b061401a534fc159ba1b03a4e3ce7469db
Kim Andre Hoff <kim@hoffet.net>
2022-08-27 01:41:48 +0200

    started with react

---
+++ resources/js/components/products/Index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Index = () => {
+    return(
+        <div className='container'>
+            <div className="products_list">
+                <div className="titlebar">
+                    <div className="titlebar_item">
+                        <h1>Products</h1>
+                    </div>
+                    <div className="titlebar_item">
+                        <div className="btn">
+                            Add Product
+                        </div>
+                    </div>
+                </div>
+
+                <div className="table">
+                    <div className="list_header">
+                        <p>Image</p>
+                        <p>Product</p>
+                        <p>Type</p>
+                        <p>Inventory</p>
+                        <p>Actions</p>
+                    </div>
+                    <div className="list_items">
+                        <img src="" height="40px" />
+                        <a>Product Name</a>
+                        <p>Category</p>
+                        <p>50</p>
+                        <div>
+                            <button className="btn-icon success">
+                                <i className="fas fa-pencil-alt"></i>
+                            </button>
+                            <button className="btn-icon danger">
+                                <i className="fas fa-trash-alt"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Index
